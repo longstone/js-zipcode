b822e0e07a63bc6417ac6cd327f56893d762caf5
longstone <git@longstone.ch>
2017-03-24 10:01:14 +0100

    add minify to build

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # js-zipcode
 Postleitzahlsuche für die Schweiz / zipcode lookup for swiss area (offline)
+
+## How to
+```
+const ZipLookup = require('./../index');
+const LookupService = new ZipLookup();
+// the return value is always  an array
+const result = LookupService.lookup(8000)[0]; 
+assert.equal(result.zip, 8000);
+```
--- src/index.js
+++ src/index.js
@@ -8,17 +8,15 @@ class ZipCodeLookpup {
     }
 
     lookup(zip) {
-        //  { "zip":9604, "loc": "Unterrindal", "canton":"SG"}
-
         return zipdatabase
             .filter(location => location.zip === zip)
             .map(location => Object.assign({}, location))
             .map(location => {
-                location['cantonLocale'] = cantonCode[location.canton];
+                location['localeCanton'] = cantonCode[location.canton];
                 if (!location.hasOwnProperty('cc')) {
                     location.cc = 'CH';
                 }
-                location['country-locale'] = countryCode[location.cc];
+                location['localeCountry'] = countryCode[location.cc];
                 return location;
             });
     }
